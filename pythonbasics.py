272a2e49205792467253e5609616ca8f693fc767
Shashang V <160448355+shashangv@users.noreply.github.com>
2024-02-21 06:59:39 +0530

    Update pythonbasics.py

--- pythonbasics.py
+++ pythonbasics.py
@@ -1,2 +1,5 @@
 # 1. Write a program which will find all such numbers which are divisible by 7 but are not a multiple of 5, between 2000 and 3200 (both included). The numbers obtained should be printed in a comma-separated sequence on a single line
 print(','.join([str(x) for x in range(2000,3201) if x%7==0 and x%5!=0]))
+
+# 2. Write a Python program to accept the user's first and last name and then getting them printed in the the reverse order with a space between first name and last name.
+print('Names reversed -->', str(input('Enter First Name : ') + ' ' + input('Enter Last Name : '))[::-1])
